73e86b994267cbe42c8ad63a0f25f62e79b9fe26
madhukarreddy5 <vanchamadhukarreddy581@gmail.com>
2024-12-12 15:20:32 -0600

    Update

--- src/App.js
+++ src/App.js
@@ -1,34 +1,43 @@
-import './App.css';
-import Cart from './pages/Cart';
-import ShopCategory from './pages/ShopCategory';
-import Product from './pages/Product';
-import Shop from './pages/Shop';
-import LoginSignup from './pages/LoginSignup'
-import { Navbar } from './components/Navbar/Navbar';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Footer from './components/Footer/Footer';
-import men_banner from './components/Asserts/banner_mens.png'
-import women_banner from './components/Asserts/banner_women.png'
-import kids_banner from './components/Asserts/banner_kids.png'
+import "./App.css";
+import Cart from "./pages/Cart";
+import ShopCategory from "./pages/ShopCategory";
+import Product from "./pages/Product";
+import Shop from "./pages/Shop";
+import LoginSignup from "./pages/LoginSignup";
+import { Navbar } from "./components/Navbar/Navbar";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Footer from "./components/Footer/Footer";
+import men_banner from "./components/Asserts/banner_mens.png";
+import women_banner from "./components/Asserts/banner_women.png";
+import kids_banner from "./components/Asserts/banner_kids.png";
 function App() {
   return (
     <div>
-     <BrowserRouter>
-      <Navbar/>
-      <Routes>
-        <Route path='/'element={<LoginSignup/>}/>
-        <Route path='/Shop'element={<Shop/>}/>
-        <Route path='/Men'element={<ShopCategory banner={men_banner} category="men"/>}/>
-        <Route path='/Women'element={<ShopCategory banner={women_banner} category="women"/>}/>
-        <Route path='/Kids'element={<ShopCategory banner={kids_banner} category="kid"/>}/>
-        <Route path='/product'element={<Product/>}>
-          <Route path=':productid'element={<Product/>}/>
-        </Route>
-        <Route path='/cart'element={<Cart/>}/>
-        
-      </Routes>
-      <Footer/>
-      </BrowserRouter> 
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<LoginSignup />} />
+          <Route path="/EcommerceFrontEnd" element={<LoginSignup />} />
+          <Route path="/Shop" element={<Shop />} />
+          <Route
+            path="/Men"
+            element={<ShopCategory banner={men_banner} category="men" />}
+          />
+          <Route
+            path="/Women"
+            element={<ShopCategory banner={women_banner} category="women" />}
+          />
+          <Route
+            path="/Kids"
+            element={<ShopCategory banner={kids_banner} category="kid" />}
+          />
+          <Route path="/product" element={<Product />}>
+            <Route path=":productid" element={<Product />} />
+          </Route>
+          <Route path="/cart" element={<Cart />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
     </div>
   );
 }
